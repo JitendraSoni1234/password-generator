8453305b28c13b549bc354e0e8e35ab10d29d825
Jitendra soni <84441277+JitendraSoni1234@users.noreply.github.com>
2024-06-01 13:52:49 +0530

    code fix

--- src/components/generator.jsx
+++ src/components/generator.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 function Generator() {
   const [password, setPassword] = useState('');
   return (
-    <div className="p-4 rounded-lg h-[70dvh] xl:w-[30dvw] lg:w-[40dvw] md:w-[50dvw] sm:w-[60dvw] flex flex-col">
+    <div className="p-4 rounded-lg h-[70dvh] w-[90dvw] xl:w-[30dvw] lg:w-[40dvw] md:w-[50dvw] sm:w-[60dvw] flex flex-col">
       <div className="text-2xl font-bold text-light text-center">Password Generator</div>
       <div className="flex flex-1 flex-col place-items-center justify-between gap-5">
         <PreviewPassword password={password} />
