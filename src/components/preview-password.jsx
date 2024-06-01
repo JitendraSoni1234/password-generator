3fff44a9d36ad94c75d150dd3fa48a3e3448bf7c
Jitendra soni <84441277+JitendraSoni1234@users.noreply.github.com>
2024-06-01 13:49:04 +0530

    copy text feature

--- src/components/preview-password.jsx
+++ src/components/preview-password.jsx
@@ -1,10 +1,10 @@
-import { MdContentCopy } from "react-icons/md";
+import { MdContentCopy } from 'react-icons/md';
 
 function PreviewPassword({ password }) {
   return (
     <div className="bg-light-dark p-4 h-[50px] w-full mt-6 flex place-items-center justify-between">
       <span className="text-light font-bold text-2xl">{password || 'P4$5W0rD!'}</span>
-      <button className="text-primary p-2 rounded-md focus:outline-none">
+      <button className="text-primary p-2 rounded-md focus:outline-none" onClick={() => navigator.clipboard.writeText(password)}>
         <MdContentCopy />
       </button>
     </div>
